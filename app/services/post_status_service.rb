9d8af8ac46f73ba39235e09d66714e16aa8ab5e5
Andy DeVane <w.andrew.dev@gmail.com>
2025-02-07 11:09:13 -0500

    add EASY_TREND var to allow statuses to trend without boosting

--- app/services/post_status_service.rb
+++ app/services/post_status_service.rb
@@ -118,6 +118,7 @@ class PostStatusService < BaseService
   def postprocess_status!
     process_hashtags_service.call(@status)
     Trends.tags.register(@status)
+    Trends.register!(@status) if ENV['EASY_TREND']
     LinkCrawlWorker.perform_async(@status.id)
     DistributionWorker.perform_async(@status.id)
     ActivityPub::DistributionWorker.perform_async(@status.id)
